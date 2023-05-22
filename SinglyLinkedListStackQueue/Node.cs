7eeb4472c882cf3a11ca287ef208401c5b482e2f
Batowei Arepua Michael <113602826+batoweiAM@users.noreply.github.com>
2023-05-22 23:08:09 +0100

    Second commit

---
+++ SinglyLinkedListStackQueue/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedListStackQueue
+{
+    public class Node<T>
+    {
+        public T Item { get; set; }
+        public Node<T>? Next { get; set; }
+        public Node<T>? Prev { get; set; }
+
+        public Node(T item)
+        {
+            Item = item;
+        }
+    }
+}
--- SinglyLinkedListStackQueue/SinglyLinkList.cs
+++ SinglyLinkedListStackQueue/SinglyLinkList.cs
@@ -1,12 +1,104 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SinglyLinkedListStackQueue
 {
-    internal class SinglyLinkList
+    public class SinglyLinkList<T> : IEnumerable<Node<T>>
     {
+        public Node<T>? Head { get; set; }
+        public int Count { get; set; }
+
+        public IEnumerator<Node<T>> GetEnumerator()
+        {
+            Node<T>? currentNode = Head;
+
+            while (currentNode != null)
+            {
+                yield return currentNode;
+                currentNode = currentNode.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(Node<T> item)
+        {
+            if (Head == null)
+            {
+                Head = item;
+            }
+            else
+            {
+                item.Next = Head;
+                Head.Prev = item;
+
+                Head = item;
+            }
+
+            Count++;
+        }
+        public bool Remove(Node<T> item)
+        {
+            if (Head == null)
+            {
+                return false;
+            }
+            if (Head.Item.Equals(item))
+            {
+                Head = Head.Next;
+                Count--;
+                return true;
+            }
+            var currentNode = Head;
+            while (!(currentNode.Next == null))
+            {
+              if (currentNode.Next.Item.Equals(item))
+                {
+                    currentNode = currentNode.Next;
+                    Count--;
+                    return true;
+                }   
+              currentNode = currentNode.Next;
+            }
+            return false;
+        }
+        public bool Check(Node<T> item)
+        {
+            var currentNode = Head;
+            while (!(currentNode == null))
+            {
+                if (currentNode.Item.Equals(item))
+                {
+                    return true;
+                }
+                currentNode = currentNode.Next;
+            }
+            return false;
+        }
+
+        public int Index(Node<T> item)
+        {
+            var nodeCount = 0;
+            var currentNode = Head;
+            while (!(currentNode == null))
+            {
+                if (currentNode.Item.Equals(item))
+                {
+                    return nodeCount;
+                }
+                nodeCount++;
+                currentNode = currentNode.Next;
+            }
+            return -1;
+        }
     }
 }
